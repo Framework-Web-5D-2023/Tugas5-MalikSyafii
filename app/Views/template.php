210270e9283ae1a85e32d2b9923c7d0988bf965f
Alfian Vito Anggoro <alfianvitoanggoro@gmail.com>
2023-10-19 09:52:20 +0700

    git and view

--- app/Views/template.php
+++ app/Views/template.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Tautan ke Bootstrap CSS -->
+  <link href="{{ ROOT_URL }}bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
   <title><?= $title ?></title>
 </head>
 
@@ -13,7 +15,7 @@
   <?= $this->renderSection('content') ?>
 </body>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{  }}bootstrap/js/bootstrap.min.js"></script>
 
 <script>
   // codingan javascript
